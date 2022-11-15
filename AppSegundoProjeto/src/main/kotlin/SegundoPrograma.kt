d53521fcabf11394e7261c7e07d682b32bd18d4c
Joolmaa <102316347+Joolmaa@users.noreply.github.com>
2022-11-15 00:32:09 -0300

    Add files via upload

---
+++ AppSegundoProjeto/src/main/kotlin/SegundoPrograma.kt
@@ -0,0 +1,15 @@
+fun main(){
+
+ var variavelA = "Programação"
+ var variavelB = "Kotlin"
+
+
+  imprimir(variavelA,variavelB)
+
+  imprimir(variavelB,variavelA)
+}
+
+fun imprimir(variavelA: String, variavelB: String) {
+
+    print("$variavelA\n$variavelB\n")
+}
